4de7033fb8b9c6248863a770fb24778007cddf96
hiren mistry <hiren.mistry@chai-monsters.com>
2014-12-04 17:47:16 -0800

    fixed import redirect error (temp)

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -9,15 +9,14 @@ class ImportsController < ApplicationController
 
     if shelters.count == 0
       msg = "Try these zipcodes: #{shelters_importer.nearby_zipcodes.uniq.join(", ")}"
-      render :index
     else
       pets_importer = PetImporter.new(petfinder_api_key)
       pets_importer.import(shelters)
 
       msg = "#{pets_importer.imported_count} pets in #{shelters.count} shelters were successfully imported."
-      redirect_to imports_path, notice: msg
     end
 
+    redirect_to imports_path, notice: msg
   end
 
   private
